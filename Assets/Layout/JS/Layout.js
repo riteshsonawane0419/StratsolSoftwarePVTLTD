7df6fe3ebd30e90ea24bca69768bb90b5957da60
riteshsonawane0419 <frontendeveloper0419@gmail.com>
2023-06-08 16:59:31 +0530

    Project

---
+++ Assets/Layout/JS/Layout.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function () {
+  InitFun();
+});
+
+function InitFun() {
+  Functions.headerCity();
+  Events.initializeEvents();
+}
+
+var Events = {
+  initializeEvents: function () {
+   
+  },
+};
+
+var Functions = {
+  headerCity: function () {
+    
+  },
+
+};
